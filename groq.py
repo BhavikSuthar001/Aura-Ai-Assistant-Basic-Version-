4b2b3d10f779384e3af25253ecd212e45f4f55d3
Bhavik Suthar <sutharbhavik54@gmail.com>
2025-11-14 22:38:28 +0530

    MegaProject (Aura_AI_Assistant)

--- groq.py
+++ groq.py
@@ -10,13 +10,3 @@ response = client.responses.create(
     model="openai/gpt-oss-20b",
 )
 print(response.output_text)
-
-
-
-
-
-
-
-    # api_key = "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"
-
-    # prompt = f"you are my personal assistant whose name is AURA dont forget and i your sir Bhavik Suthar. make sure when i give you any task or command than give that ans in 2 - 3 sentence in simple form not in long also talk with me like an freind. i am an btech in Ai and data science student. be my assistant who always talk with me like jarvis from iron man movie. talk me in only english language and be nice to me. Also you dont have to say my surname only bhavik and dont talk about my degree just follow my work which i am going to give you and also you dont have to say about you that you are aura only say the answer of my question : {command}"
